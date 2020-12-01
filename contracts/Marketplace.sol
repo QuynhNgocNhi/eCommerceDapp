5e88e4d7661ad8692096e38cdb1a645dfeaede84
Gerwin Fricke <gerwin.fricke@gmail.com>
2020-12-01 12:05:46 +0100

    implemented circuit breaker

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -7,6 +7,7 @@ contract Marketplace {
   address payable owner;
   uint productCount;
   mapping (uint => Product) public products;
+  bool isActive = true;
 
   struct Product {
     uint id;
@@ -27,6 +28,7 @@ contract Marketplace {
   event LogForSale(uint id);
   event LogSold(uint id);
 
+  modifier contractIsActive() { require(isActive == true); _;}
   modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
   modifier checkValue(uint _id) {
     _;
@@ -34,8 +36,6 @@ contract Marketplace {
     uint amountToRefund = msg.value - _price;
     products[_id].buyer.transfer(amountToRefund);
   }
-
-
   modifier forSale(uint _id) { require(products[_id].state == State.ForSale && products[_id].buyer == address(0)); _;}
   modifier sold(uint _id) { require(products[_id].state == State.Sold); _;}
 
@@ -44,11 +44,16 @@ contract Marketplace {
     productCount = 0;
   }
 
+  function toggleCircuitBreaker() external {
+      require(owner == msg.sender);
+      isActive = !isActive;
+  }
+
   function getCount() public view returns (uint id) {
     return productCount;
   }
 
-  function addProduct(string memory _name, uint _price) public returns(bool){
+  function addProduct(string memory _name, uint _price) contractIsActive() public returns(bool) {
     emit LogForSale(productCount);
     products[productCount] = Product({name: _name, id: productCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
     productCount += 1;
@@ -58,7 +63,7 @@ contract Marketplace {
   using SafeMath for uint;
 
   function buyProduct(uint id)
-    public payable forSale(id) paidEnough(products[id].price) checkValue(id)
+    public payable forSale(id) paidEnough(products[id].price) checkValue(id) contractIsActive()
   {
     products[id].buyer = msg.sender;
     uint commission = SafeMath.div(products[id].price, 20);
